704acebe25f4c58ac37442a1bfbc2deb08270e63
irfandi marsya <join.mantap@gmail.com>
2025-06-25 23:25:11 +0700

    feat(handler): add support for parsing custom URLs via query param
    Add ability to parse a URL provided through 'url' query parameter instead of using the request URL. Improve error message when URL parsing fails and include the parsed URL in the response.

--- container_src/main.go
+++ container_src/main.go
@@ -11,15 +11,22 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	message := os.Getenv("MESSAGE")
 	instanceId := os.Getenv("CLOUDFLARE_DEPLOYMENT_ID")
-	
-	// Parse the URL to ensure it's valid
-	parsedURL, err := url.Parse(r.URL.String())
-	if err != nil {
-		http.Error(w, "Invalid URL", http.StatusBadRequest)
+
+	urlToParse := r.URL.Query().Get("url")
+	if urlToParse == "" {
+		fmt.Fprintf(w, "Hi, I'm a container and this is my message: \"%s\", my instance ID is: %s, path: %s. Provide a `url` query parameter to parse a different URL.", message, instanceId, r.URL.Path)
 		return
 	}
-	
-	fmt.Fprintf(w, "Hi, I'm a container and this is my message: \"%s\", my instance ID is: %s, path: %s", message, instanceId, parsedURL.Path)
+
+	// Parse the URL to ensure it's valid
+	log.Printf("Parsing URL: %s", urlToParse)
+	parsedURL, err := url.Parse(urlToParse)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error parsing URL '%s': %v", urlToParse, err), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "Hi, I'm a container and this is my message: \"%s\", my instance ID is: %s, parsed path from '%s' is: %s", message, instanceId, urlToParse, parsedURL.Path)
 }
 
 func errorHandler(w http.ResponseWriter, r *http.Request) {
